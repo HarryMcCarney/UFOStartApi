d5c46d2f881d378cf407d4d5fc4367073410d5d2
Harry McCarney <hmccarney@gmail.com>
2013-05-24 18:07:40 +0200

    added linkedin in secret

--- Model/Profile.cs
+++ Model/Profile.cs
@@ -22,6 +22,8 @@ namespace Model
         public string email { get; set; }
         [XmlAttribute]
         public string name { get; set; }
+        [XmlAttribute]
+        public string secret { get; set; }
     }
 
    
