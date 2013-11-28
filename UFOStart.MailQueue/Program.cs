4e8d2b00e9f1706b390b23de4ed3a0b31139c8ac
Harry McCarney <hmccarney@gmail.com>
2013-11-28 18:11:24 +0100

    added some commen

--- UFOStart.MailQueue/Program.cs
+++ UFOStart.MailQueue/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HackandCraft.Config;
+﻿using HackandCraft.Config;
 using Topshelf;
 using mandrill.net;
+
 
 namespace UFOStart.MailQueue
 {
